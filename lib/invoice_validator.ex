c08a7b7e7596edc74b39aaefac8b3e970b5ddb04
Caleb J. Acosta <keyboardkiller.gk@gmail.com>
2022-03-28 22:14:45 -0600

    Taller Testing

---
+++ lib/invoice_validator.ex
@@ -0,0 +1,24 @@
+defmodule InvoiceValidator do
+  def validate_dates(%DateTime{} = emisor_dt, %DateTime{} = pac_dt) do   
+    case DateTime.compare(emisor_dt, pac_dt) do 
+      :gt ->
+        cond do
+          DateTime.diff(emisor_dt, pac_dt) <= 300 ->
+            :ok
+          true ->
+            "Invoice is more than 5 mins ahead in time"
+        end
+      :lt ->
+        cond do
+          DateTime.diff(pac_dt, emisor_dt) <= 259200 ->
+            :ok
+          true -> 
+            "Invoice was issued more than 72 hrs before received by the PAC"
+        end
+      :eq ->
+        :ok
+        
+    end
+  end
+end
+
---
+++ test/invoice_validator.exs
@@ -0,0 +1,41 @@
+defmodule InvoiceValidator3Test do
+  use ExUnit.Case, async: false
+  import InvoiceValidator
+
+  Calendar.put_time_zone_database(Tzdata.TimeZoneDatabase)
+
+  @pac_dt DateTime.from_naive!(~N[2022-03-23 15:06:35], "America/Mexico_City")
+
+  data = [
+    {"72 hrs atras",    "America/Tijuana",      ~N[2022-03-20 13:06:31],  "Invoice was issued more than 72 hrs before received by the PAC"},
+    {"72 hrs atras",    "America/Mazatlan",     ~N[2022-03-20 14:06:31],  "Invoice was issued more than 72 hrs before received by the PAC"},
+    {"72 hrs atras",    "America/Mexico_City",  ~N[2022-03-20 15:06:31],  "Invoice was issued more than 72 hrs before received by the PAC"},
+    {"72 hrs atrás",    "America/Cancun",       ~N[2022-03-20 16:06:31],  "Invoice was issued more than 72 hrs before received by the PAC"},
+    {"72 hrs atras",    "America/Tijuana",      ~N[2022-03-20 13:06:35],  :ok},
+    {"72 hrs atras",    "America/Mazatlan",     ~N[2022-03-20 14:06:35],  :ok},
+    {"72 hrs atras",    "America/Mexico_City",  ~N[2022-03-20 15:06:35],  :ok},
+    {"72 hrs atras",    "America/Cancun",       ~N[2022-03-20 16:06:35],  :ok},
+    {"5 mns adelante",  "America/Tijuana",      ~N[2022-03-23 13:11:35],  :ok},
+    {"5 mns adelante",  "America/Mazatlan",     ~N[2022-03-23 14:11:35],  :ok},
+    {"5 mns adelante",  "America/Mexico_City",  ~N[2022-03-23 15:11:35],  :ok},
+    {"5 mns adelante",  "America/Cancun",       ~N[2022-03-23 16:11:35],  :ok},
+    {"5 mns adelante",  "America/Tijuana",      ~N[2022-03-23 13:11:36],  "Invoice is more than 5 mins ahead in time"},
+    {"5 mns adelante",  "America/Mazatlan",     ~N[2022-03-23 14:11:36],  "Invoice is more than 5 mins ahead in time"},
+    {"5 mns adelante",  "America/Mexico_City",  ~N[2022-03-23 15:11:36],  "Invoice is more than 5 mins ahead in time"},
+    {"5 mns adelante",  "America/Cancun",       ~N[2022-03-23 16:11:36],  "Invoice is more than 5 mins ahead in time"}
+  ]
+
+  for {text, emisor_time_zone, emisor_date_time, result} <- data do
+    @text text
+    @emisor_date_time emisor_date_time
+    @emisor_time_zone emisor_time_zone
+    @result result
+    test "#{@text}, emisor in #{@emisor_time_zone} at #{@emisor_date_time} returns #{@result}" do
+      # Change test implementation
+      assert validate_dates(
+        DateTime.from_naive!(@emisor_date_time, @emisor_time_zone),
+        @pac_dt
+      ) == @result
+    end
+  end
+end
